df04507ab99914b4a0421aec6f15212f417aa178
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-22 15:31:34 -0700

    Player bounces after hitting enemy

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -11,18 +11,21 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 	
 func _process(_delta: float) -> void:
 	Input.set_custom_mouse_cursor(lettuce_cursor, Input.CURSOR_ARROW)
+
+func jump():
+	velocity.y = 300.0 
