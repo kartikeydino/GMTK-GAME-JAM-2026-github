1c5fd423d7164c19223f9f8b690c1d762a4a956b
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-23 17:41:17 -0700

    New Player Stuff

--- Scripts/new_player.gd
+++ Scripts/new_player.gd
@@ -13,7 +13,9 @@ extends CharacterBody2D
 @export var dash_counter = 0 
 #Connections
 @onready var jump_buffer : Area2D
-@onready var dash_timer : Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_polygon_2d: CollisionShape2D = $CollisionPolygon2D
+
 
 func _physics_process(delta: float) -> void:
 	if not jump_buffer.has_overlapping_areas():
@@ -23,8 +25,10 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 	if direction and has_walk == true:
 		velocity.x = direction * speed
+		animated_sprite_2d.play("running")
 	else:
 		velocity.x = 0 
+		animated_sprite_2d.play("default")
 	other_movement()
 	move_and_slide()
 func other_movement():
@@ -33,7 +37,18 @@ func other_movement():
 	if Input.is_action_just_pressed("dash") and has_dash == true:
 		speed = dash_speed 
 		dash_counter += 1 
+		print(dash_counter)
 		await get_tree().create_timer(0.12).timeout
 		speed = 300.0 
 	else: 
 		speed = 300.0 
+	if dash_counter == 3:
+		collision_polygon_2d.disabled = true 
+		animated_sprite_2d.modulate.a = 200 
+		speed = (dash_speed * 2)
+		await get_tree().create_timer(0.12).timeout 
+		speed = 300.0 
+	else:
+		collision_polygon_2d.disabled = false
+		animated_sprite_2d.modulate.a = 255  
+		speed = 300.0
