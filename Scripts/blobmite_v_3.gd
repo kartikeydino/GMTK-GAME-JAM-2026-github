ebea76186ed582b58299217f13118b5750cd1dfd
kartikeydino <kartikeydino@gmail.com>
2026-07-24 16:13:21 -0700

    did random stuff...

--- Scripts/blobmite_v_3.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-var ant_enemy: Area2D
-@export var speed: int = 1
-var tween = create_tween()
-
-func _process(_delta: float) -> void:
-	tween.tween_property(self, "global_position", Vector2(ant_enemy.global_position), speed)
