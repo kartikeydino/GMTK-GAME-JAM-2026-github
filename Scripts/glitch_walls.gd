6cef6d7006d0af576455416575f0b70c30a52a7b
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-24 14:17:14 -0700

    YOU CAN GO THROUGH WALLS WITH GLITCH FLING

--- Scripts/glitch_walls.gd
+++ Scripts/glitch_walls.gd
@@ -4,10 +4,12 @@ var player
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
 		player = body
-		player.animated_sprite_2d.play("idle")
+		player.animated_sprite_2d.play("running")
 		player.set_physics_process(false)
 		await get_tree().create_timer(0.5).timeout
 		player.speed = player.normal_speed * 10
+		player.collision_shape.disabled = true 
 		player.set_physics_process(true)
-		await get_tree().create_timer(3).timeout
+		await get_tree().create_timer(0.12).timeout
 		player.speed =  player.normal_speed
+		player.collision_shape.disabled = false 
