4d10d550a86d9869a7af9fd15e27465e3995117d
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-24 10:00:36 -0700

    Changed dash speed

--- Scripts/new_player.gd
+++ Scripts/new_player.gd
@@ -9,7 +9,7 @@ var dying = false
 #Movement 
 @export var speed = 600.0 
 @export var normal_speed = 600.0
-@export var dash_speed = 1200.0
+@export var dash_speed = 3000.0
 @export var jump = -800.0 
 @export var downward_force = 1.3 
 var walking: bool = false
@@ -32,7 +32,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("climb") and has_wall_climb == true and is_on_wall():
 		velocity.y = jump
 	
-	if Input.is_action_just_pressed("phase"):
+	if Input.is_action_pressed("phase"):
 		animated_sprite_2d.modulate.a = 200
 
 	var direction := Input.get_axis("left", "right")
@@ -49,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("dash") and has_dash:
 		speed = dash_speed
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.15).timeout
 		speed = normal_speed
 	
 	#if velocity.x >= 300 && is_on_wall_only():
