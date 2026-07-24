e27322886395fced70f0e376a9f654810ae76dbf
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-24 11:50:56 -0700

    Changed spider hitboxes

--- spider.gd
+++ spider.gd
@@ -5,9 +5,3 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.global_position = Vector2(-5.0, -394.0)
-
-
-
-func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		queue_free()
