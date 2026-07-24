06dcbe843e8b4f2bb488d0cb24f14938717040c5
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-24 11:39:57 -0700

    Level 2

--- spider.gd
+++ spider.gd
@@ -3,11 +3,11 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_node("CharacterBody2D"):
+	if body.name == "Player":
 		body.global_position = Vector2(-5.0, -394.0)
 
 
 
 func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.has_node("CharacterBody2D"):
+	if body.name == "Player":
 		queue_free()
