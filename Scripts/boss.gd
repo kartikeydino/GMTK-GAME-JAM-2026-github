6b5f2d68743cf727ae38e80f608d89682360953d
anaysahani12-cloud <anaysahani12@gmail.com>
2026-07-25 11:23:39 -0700

    Boss update

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -6,6 +6,10 @@ enum States {IDLE, ATTACK_1, ATTACK_2, ATTACK_3}
 @export var health = 4 
 @onready var player: CharacterBody2D = $"../Player"
 @onready var health_bar: ProgressBar = $ProgressBar
+@onready var screw: Area2D = $Screw
+@onready var screw_2: Area2D = $Screw2
+@onready var screw_3: Area2D = $Screw3
+@onready var screw_4: Area2D = $Screw4
 
 
 func decrease_health():
@@ -13,27 +17,31 @@ func decrease_health():
 	health -= 1
 	health_bar.value -= health_bar.step
 	if health == 0: 
-		queue_free()
+		screw.queue_free()
 
 func _on_screw_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and player.speed == 6000.0:
+	if body.name == "Player":
 		decrease_health()
+		screw_2.queue_free()
 
 
 func _on_screw_2_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
+		screw_3.queue_free()
 
 
 func _on_screw_3_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
+		screw_4.queue_free()
 		
 
 
 func _on_screw_4_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
+		queue_free()
 
 func _process(delta: float) -> void:
 	attacks()
