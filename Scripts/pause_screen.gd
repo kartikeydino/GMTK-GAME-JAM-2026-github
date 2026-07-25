ea3238d2bb22abe1e995a6c500349b2e2c74dc33
kartikeydino <kartikeydino@gmail.com>
2026-07-25 16:06:00 -0700

    WE'RE DONE!!!!!!!!!!!!!!!!!!

--- Scripts/pause_screen.gd
+++ Scripts/pause_screen.gd
@@ -1,6 +1,6 @@
 extends Control
-
-var audio_bus = AudioServer.get_bus_index("BG music")
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var h_slider: HSlider = $HSlider
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,11 +11,20 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		visible = true
 		get_tree().paused = true
-	
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("BG music"), h_slider.value)
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("VFX"), h_slider.value)
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), h_slider.value)
 
 
 func _on_mute_toggled(toggled_on: bool) -> void:
-	pass
+	if toggled_on:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BG music"), true)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("VFX"), true)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), true)
+	if toggled_on == false:
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BG music"), false)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("VFX"), false)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
 
 
 func _on_button_pressed() -> void:
@@ -24,4 +33,4 @@ func _on_button_pressed() -> void:
 
 
 func _on_blob_pressed() -> void:
-	pass # Replace with function body.
+	audio_stream_player.play()
