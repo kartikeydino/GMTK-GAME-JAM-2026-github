fa5e234fbd0363a08eda330fee9cf57bc117a535
kartikeydino <kartikeydino@gmail.com>
2026-07-25 15:10:31 -0700

    fixed it

--- Scripts/main_level_3.gd
+++ Scripts/main_level_3.gd
@@ -30,7 +30,7 @@ func _on_finish_button_2_pressed() -> void:
 
 
 func _on_boss_died() -> void:
-	if timer.time_left < 0:
+	if timer.timer.time_left < 0:
 		losing_label.visible = false
 		finish_button_2.visble = true
 		extra_blobl_level.visible = true
