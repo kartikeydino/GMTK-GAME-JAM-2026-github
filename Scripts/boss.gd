73778ee2758450b37713273dfa6658c4663c6aa8
kartikeydino <kartikeydino@gmail.com>
2026-07-25 11:31:34 -0700

    fixed something?

--- Scripts/boss.gd
+++ Scripts/boss.gd
@@ -6,7 +6,10 @@ enum States {IDLE, ATTACK_1, ATTACK_2, ATTACK_3}
 @export var health = 4 
 @onready var player: CharacterBody2D = $"../Player"
 @onready var health_bar: ProgressBar = $ProgressBar
-
+@onready var screw: Area2D = $Screw
+@onready var screw_2: Area2D = $Screw2
+@onready var screw_3: Area2D = $Screw3
+@onready var screw_4: Area2D = $Screw4
 
 func decrease_health():
 	
@@ -18,26 +21,26 @@ func decrease_health():
 func _on_screw_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
-		queue_free()
+		screw.queue_free()
 
 
 func _on_screw_2_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
-		queue_free()
+		screw_2.queue_free()
 
 
 func _on_screw_3_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
-		queue_free()
+		screw_3.queue_free()
 		
 
 
 func _on_screw_4_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		decrease_health()
-		queue_free()
+		screw_4.queue_free()
 
 func _process(delta: float) -> void:
 	attacks()
