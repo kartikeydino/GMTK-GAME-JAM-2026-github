202e0d8964a0db2a585ec05ddd515258d9f7937b
kartikeydino <kartikeydino@gmail.com>
2026-07-25 09:00:36 -0700

    idk

--- Scripts/main_level_1.gd
+++ Scripts/main_level_1.gd
@@ -31,4 +31,4 @@ func _on_jump_enemy_died() -> void:
 	
 
 func _on_lvl_2_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_2_main.tscn")
